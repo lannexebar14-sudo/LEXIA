bb30883aaf9705cddb47dec7145a0c790b3ba2a0
lannexebar14-sudo <lannexebar14@gmail.com>
2026-08-04 23:27:01 +0200

    Active le nouvel en-tête mobile client

--- lexia-starter/app/layout.tsx
+++ lexia-starter/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./mobile-polish.css";
 import "./administration/mobile-header.css";
+import "./tableau-de-bord/client-mobile-header.css";
 import "./nouveau-dossier/stripe-checkout.css";
 import LiveSupport from "./components/LiveSupport";
 import PwaExperience from "./components/PwaExperience";
