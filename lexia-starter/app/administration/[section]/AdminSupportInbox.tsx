569049e54b4c2e4e4cba32fc3ee451cd0e32a346
lannexebar14-sudo <lannexebar14@gmail.com>
2026-08-04 15:07:21 +0200

    Charge le design de la messagerie d'assistance

--- lexia-starter/app/administration/[section]/AdminSupportInbox.tsx
+++ lexia-starter/app/administration/[section]/AdminSupportInbox.tsx
@@ -2,6 +2,7 @@
 
 import { FormEvent, useEffect, useMemo, useState } from "react";
 import { createClient } from "../../../lib/supabase/client";
+import "./admin-support.css";
 
 type SupportRow = {
   id: string;
