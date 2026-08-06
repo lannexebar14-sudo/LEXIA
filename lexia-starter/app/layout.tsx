9b3f96c85510e52ff542cce4f8c0c38cdd061a65
lannexebar14-sudo <lannexebar14@gmail.com>
2026-08-06 15:53:36 +0200

    Charge les correctifs d’inscription et de paiement

--- lexia-starter/app/layout.tsx
+++ lexia-starter/app/layout.tsx
@@ -8,6 +8,7 @@ import "./administration/dashboard-polish.css";
 import "./tableau-de-bord/client-mobile-header.css";
 import "./nouveau-dossier/stripe-checkout.css";
 import "./nouveau-dossier/mobile-header-fix.css";
+import "./payment-enforcement.css";
 import LiveSupport from "./components/LiveSupport";
 import PwaExperience from "./components/PwaExperience";
 import MobilePlatformBadge from "./components/MobilePlatformBadge";
